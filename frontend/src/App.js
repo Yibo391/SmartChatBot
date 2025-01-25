20f5972e5ee121d54cd0485958caea760fa926f7
Yibo391 <391583391@qq.com>
2025-01-25 12:04:35 +0100

    init frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <header>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+
+          hello guys 
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
